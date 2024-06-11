482bce382b5b6e2825d2f6ce16ddcd4061982132
Tom090 <twalne91@gmail.com>
2024-06-11 12:38:50 +0100

    commit

---
+++ src/list_foundation_models_ddl/input.ts
@@ -0,0 +1,12 @@
+export type ListFoundationModelsDdlInput = {
+	/**
+	 * @title Max Results
+	 * @description The maximum number of results to return.
+	 */
+	maxResults?: number;
+	/**
+	 * @title Next Token
+	 * @description A token to specify where to start paginating.
+	 */
+	nextToken?: string;
+};
